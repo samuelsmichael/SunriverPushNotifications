a4684b04c52dd75d6071e21832c474fcb226c3e9
Mike Samuels <samuelsmichael222@gmail.com>
2016-01-04 06:08:53 -0700

    IN PROGRESS (1/4/2016)

---
+++ Common/DBController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Common {
+    public class DBController {
+        #region Variables
+        private String mSessionId;
+        #endregion
+        #region Constructor
+        public DBController(string sessionId) {
+            mSessionId = sessionId;
+        }
+        #endregion
+        #region Public Interface
+        public void setPushNotificationParameters(ref bool? isPushNotificationEnabled, ref int? periodicityInMinutes) {
+            SqlConnection conn = null;
+            SqlCommand cmd = null;
+            try {
+                conn = new SqlConnection(CommonMethods.getConnectionString(ConfigurationManager.AppSettings["SROCalendarProvider"]));
+                conn.Open();                
+                cmd = new SqlCommand("SRSSynchronizationControlGetAndSet", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                if (isSynchronizationEnabled.HasValue) {
+                    cmd.Parameters.Add("@IsSynchronizationActive", SqlDbType.Bit).Value = isSynchronizationEnabled.Value;
+                }
+                if (periodicityInMinutes.HasValue) {
+                    cmd.Parameters.Add("@SynchronizationPeriodicityInMinutes", SqlDbType.Int).Value = periodicityInMinutes.Value;
+                }
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                if (CommonRoutines.HasData(ds)) {
+                    periodicityInMinutes = CommonRoutines.ObjectToInt(ds.Tables[0].Rows[0]["SynchronizationPeriodicityInMinutes"]);
+                    isSynchronizationEnabled = CommonRoutines.ObjectToBool(ds.Tables[0].Rows[0]["IsSynchronizationActive"]);
+                }
+            } catch (Exception e) {
+            } finally {
+                try { cmd.Dispose(); } catch { }
+                try { conn.Close(); } catch { }
+            }
+        }
+        public void getPushNotificationParameters(
+                out bool isPushNotificationEnabled, 
+                out int periodicityInMinutes) {
+            periodicityInMinutes = 60;
+            isPushNotificationEnabled = true;
+        }
+        public void pushNotification() {
+            SqlConnection conn = null;
+            SqlCommand cmd = null;
+            try {
+                conn = new SqlConnection(getConnectionString(ConfigurationManager.AppSettings["PushAlerts"]));
+            } finally {
+            }
+        }
+        #endregion
+
+        #region Private Interface
+        private string getConnectionString(string name) {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+        #endregion
+    }
+}
---
+++ Common/Scheduler.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace Common {
+    public class Scheduler {
+        #region Variables
+        public EventLog windowsLog = new EventLog("SR Push Notifications");
+        private Timer mRefreshTimer = null;
+        private int mPeriodicityInMinutes;
+        private bool mIsSynchronizationEnabled;
+        private DBController mDBController;
+        private string mSessionId;
+        private string mSource;
+        private bool mWriteLogEntries;
+        #endregion
+        #region Properties
+
+        public int PeriodicityInMinutes {
+            get { return mPeriodicityInMinutes; }
+            set { mPeriodicityInMinutes = value; }
+        }
+
+        public bool IsSynchronizationEnabled {
+            get {
+                return mIsSynchronizationEnabled;
+            }
+            set {
+                mIsSynchronizationEnabled = value;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sessionId">If comming from a web app, pass its SessionId string value,
+        ///     otherwise, pass in any value.</param>
+        /// <param name="source">Either the web app or the windows service</param>
+        public Scheduler(string sessionId, string source) {
+            mSessionId=sessionId;
+            mSource = source;
+            mDBController = new DBController(mSessionId);
+            mWriteLogEntries =
+                CommonRoutines.ObjectToBool(ConfigurationManager.AppSettings["WriteLogEntries"]);
+        }
+
+
+        #endregion
+
+        #region Public Interface
+        /// <summary>
+        /// Perform startup functions
+        /// </summary>
+        public void Start() {
+            // fetch the latest control values
+            mDBController.getSynchronizationParameters(out mIsSynchronizationEnabled, out mPeriodicityInMinutes);
+            startTimer();
+        }
+        /// <summary>
+        /// Perform shutdown functions
+        /// </summary>
+        public void Stop() {
+            stopTimer(true);
+        }
+
+        ///
+        public void writeEventLogEntry(string entry) {
+            if (mWriteLogEntries) {
+                windowsLog.Source = mSource;
+                try {
+                    windowsLog.WriteEntry(entry);
+                } catch { }
+            }
+        }
+
+        #endregion
+
+        #region Private Interface
+        /// <summary>
+        /// When starting the timer, set it so that it only pops once.  Then
+        /// when it pops, start it again.
+        /// </summary>
+        private void startTimer() {
+            stopTimer(false);
+            double milliSeconds=mPeriodicityInMinutes*60*1000;
+            writeEventLogEntry("Starting timer loop for every: "+mPeriodicityInMinutes+ " minutes.");
+            /*The first time, we do it right away*/
+            milliSeconds = 1000;
+            mRefreshTimer = new System.Timers.Timer(milliSeconds);
+            mRefreshTimer.AutoReset = false;
+            mRefreshTimer.Elapsed += new System.Timers.ElapsedEventHandler(mRefreshTimer_Elapsed);
+            mRefreshTimer.Start();
+        }
+        /// <summary>
+        /// Timer has "popped".  
+        /// Synchronize database, and then re-set the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mRefreshTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e) {
+            if (mIsSynchronizationEnabled) {
+                writeEventLogEntry("Timer popped");
+                mDBController.synchronizeData();
+            }
+            // update control values, in case they've changed
+            mDBController.getSynchronizationParameters(out mIsSynchronizationEnabled, out mPeriodicityInMinutes);
+            mRefreshTimer.Interval = mPeriodicityInMinutes * 60 * 1000;
+            // reset the timer
+            mRefreshTimer.Start();
+        }
+        /// <summary>
+        /// Stop the timer, if it's running
+        /// </summary>
+        private void stopTimer(bool writeLog) {
+            if (writeLog) {
+                writeEventLogEntry("Stopping timer loop.");
+            }
+            if (mRefreshTimer != null) {
+                try {
+                    mRefreshTimer.Stop();
+                    mRefreshTimer.Dispose();
+                } catch (Exception e) {
+                }
+            }
+            mRefreshTimer = null;
+        }
+        #endregion
+    }
+}
